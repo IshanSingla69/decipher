ba38da91a9efef98a3cd6545a9046aa9d6d753a1
Siddharth Sharma <siddharthhustler@gmail.com>
2024-06-02 11:04:00 +0530

    dashboard

--- decipher/bot_home/views.py
+++ decipher/bot_home/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
 # Create your views here.
 def chatbot_view(request):
-
+    if request.method == 'POST':
+        prompt = request.POST.get('prompt')
+        print(prompt)
+        if prompt:
+            return redirect("bot_home/success.html")
     return render(request, "bot_home/chatbot.html")
