6e65b08f282d28a703ae9ee1f96493d887c51c60
ishan singla <singlaishan69@gmail.com>
2024-06-05 16:26:53 +0530

    get user data

--- decipher/oauth/models.py
+++ decipher/oauth/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 
 class ApiKeys(models.Model):
     user_mail = models.OneToOneField(User, on_delete=models.CASCADE)
-    key_id = models.CharField(max_length=255)
-    key_page = models.CharField(max_length=255)
+    integration_key = models.CharField(max_length=255)
+    page_id = models.CharField(max_length=255)
     def __str__(self):
         return f"{self.user.username}"
 
