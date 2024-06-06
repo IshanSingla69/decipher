6c237e269f24b385296aaa4ad302d0490cbe7dea
ishan singla <singlaishan69@gmail.com>
2024-06-06 13:14:46 +0530

    html

--- decipher/bot_home/decipher_bot/telegram_bot/TelegramBot.py
+++ decipher/bot_home/decipher_bot/telegram_bot/TelegramBot.py
@@ -2,13 +2,15 @@ from enum import Enum, auto
 from typing import Final
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+# from .events import Publisher, Subscriber
+
 
 TOKEN :Final = '7448033384:AAFAebODHPDaVkDqS-XgS5u9KTOmiGf5YYw'
 BOT_USERNAME :Final = '@thedecipher_bot'
-
 class UserState(Enum):
     AWAITING_INTEGRATION_TOKEN = auto()
     AWAITING_PAGE_ID = auto()
+    CONFIRMING_INPUT = auto()
 
 class BotCommands(Enum):
     START = 'start'
@@ -20,7 +22,6 @@ integration_token: str = ''
 page_id: str = ''
 
 user_state = {}
-
 
 # Commands
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -45,9 +46,22 @@ async def set_page_id_command(update: Update, context: ContextTypes.DEFAULT_TYPE
 
     print(user_state[update.message.from_user.id])
 
+async def confirm_details(update: Update, _integration_token: str, _page_id: str):
+    user_id = update.message.from_user.id
+    user_state[user_id] = UserState.CONFIRMING_INPUT
+    confirmation_txt = f'Integration token is: {_integration_token}\nPage ID: {_page_id}.\nIs this correct? (yes/no)'
+    await update.message.reply_text(confirmation_txt)
+
+# async def notify_details_confirmed(update: Update, context: ContextTypes.DEFAULT_TYPE):
+#     Publisher.dispatch('details_confirmed', integration_token, page_id)
+#     await update.message.reply_text('Details notified')
+
 # Responses
 
 async def handle_messages(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    global integration_token
+    global page_id
 
     user_id = update.message.from_user.id
     text = update.message.text
@@ -55,32 +69,39 @@ async def handle_messages(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if user_id in user_state:
         state = user_state[user_id]
         print(state)
-        # if state == UserState.AWAITING_INTEGRATION_TOKEN:
-        #     global integration_token
-        #     integration_token = text
-        #     await update.message.reply_text(f'Your Notion integration token has been set to: {integration_token}')
-        #     print(f'Your Notion integration token has been set to: {integration_token}')
-        #     user_state.pop(user_id) #Clear the state
-        # else:
-        #     print(state)
-        #     await update.message.reply_text("I don't understand your request.")
+        
         match state:
             case UserState.AWAITING_INTEGRATION_TOKEN:
-                global integration_token
                 integration_token = text
                 await update.message.reply_text(f'Your Notion integration token has been set to: {integration_token}')
                 print(f'Your Notion integration token has been set to: {integration_token}')
-                user_state.pop(user_id) #Clear the state
+                if page_id:
+                    await confirm_details(update, integration_token, page_id)
+                else:
+                    user_state.pop(user_id)
 
             case UserState.AWAITING_PAGE_ID:
-                global page_id
                 page_id = text
                 await update.message.reply_text(f'Your Notion Page ID has been set to: {page_id}')
                 print(f'Your Notion Page ID has been set to: {page_id}')
-                user_state.pop(user_id) #Clear the state
+                if integration_token:
+                    await confirm_details(update, integration_token, page_id)
+                else:
+                    user_state.pop(user_id)
             
+            case UserState.CONFIRMING_INPUT:
+                if text.lower() == 'yes':
+                    user_state.pop(user_id)
+                    print('Details Confirmed')
+                    # Publisher.dispatch('details_confirmed', integration_token, page_id)
+                    await update.message.reply_text('Great! You can start using decipher bot now, just enter a prompt and I will create a notion page for you.')
+                elif text.lower() == 'no':
+                    user_state.pop(user_id)
+                    await update.message.reply_text('Please set your integration token and page id again.')
+
             case _:
                 print(state)
+                user_state.pop(user_id)
                 await update.message.reply_text("I don't understand your request.")
 
     else:
@@ -97,6 +118,7 @@ def main():
     app.add_handler(CommandHandler('help', help_command))
     app.add_handler(CommandHandler('set_integration_token', set_integration_token_command))
     app.add_handler(CommandHandler('set_page_id', set_page_id_command))
+    # app.add_handler(CommandHandler('notify_details', notify_details_confirmed))
 
     # Add message Handler
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_messages))
--- decipher/bot_home/views.py
+++ decipher/bot_home/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .decipher_bot import decipher
-from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+import json
+# from .decipher_bot.telegram_bot.events import Publisher, Subscriber
 
 notion_page_id = "3411ec690b544b14bf57b99eb052f3e4"
 integeration_token = "secret_ni7kJlPLPcaPmVlZCgXG2QDmz0xR7p1SL9Mg2MWTh52"
+
+notion_page_id:str = ""
+integeration_token:str = ""
+details_confirmed:bool = False
+
+
+
+# def set_user_details(_integeration_token, _notion_page_id):
+#     global notion_page_id
+#     global integeration_token
+#     global details_confirmed
+#     notion_page_id = _notion_page_id
+#     integeration_token = _integeration_token
+#     details_confirmed = True
+#     redirect("chatbot_view")
+
+# set_user_details_subscriber = Subscriber('details_confirmed', set_user_details)
+# Publisher.register('details_confirmed', set_user_details_subscriber)
 
 def chatbot_view(request):
     if request.method == 'POST':
@@ -18,6 +39,36 @@ def chatbot_view(request):
             }
             return render(request, "bot_home/chatbot.html", context)
     return render(request, "bot_home/chatbot.html")
+    # if details_confirmed:
+    #     if request.method == 'POST':
+    #         prompt = request.POST.get('user-prompt')
+    #         print(prompt)
+    #         if prompt:
+    #             decipher.CreateNotionPage(notion_page_id, integeration_token, prompt)
+    #             context ={
+    #                 'prompt':prompt,
+    #                 'success': True
+    #             }
+    #             return render(request, "bot_home/chatbot.html", context)
+    #     return render(request, "bot_home/chatbot.html")
+    # else:
+    #     return HttpResponse("Please set your integration token and page id first " + details_confirmed.__str__() + " page id: "+ notion_page_id + " integration token " + integeration_token)
+    
+
+def dashboard_view(request):
+    if request.method == 'POST':
+        prompt = request.POST.get('user-prompt')
+        print(prompt)
+        if prompt:
+            decipher.CreateNotionPage(notion_page_id, integeration_token, prompt)
+            context ={
+                'prompt':prompt,
+                'success': True
+            }
+            return render(request, "bot_home/dashboard.html", context)
+        return redirect("chatbot_view")
+    return render(request, "bot_home/dashboard.html")
+
 
 def success_view(request):
     return redirect("chatbot_view")
